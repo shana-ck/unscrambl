85a2887b5091248963d4265b550ced1cd70eea33
shana-ck <s.klein1127@gmail.com>
2026-01-14 13:52:27 -0500

    updates

--- class.js
+++ class.js
@@ -1,4 +1,4 @@
-import words from './sanitized.json' with {type: 'json'}
+import words from './wordList.json' with {type: 'json'}
 
 const shufButton = document.getElementById('shuffle')
 const start = document.getElementById('start')
@@ -6,8 +6,21 @@ const input = document.getElementById('input')
 const form = document.getElementById('guess')
 const display = document.getElementById('countdown')
 const levelBtn = document.getElementById('level-up')
-const container = document.querySelector('.input-container')
 const debugContainer = document.querySelector('.debug')
+const debugBtn = document.getElementById("debugBtn")
+const stopBtn = document.getElementById('stopBtn')
+const threshold = document.getElementById('threshold')
+const remaining = document.getElementById('remaining')
+const scoreInfo = document.getElementById('level-score')
+const wordsThreshold = document.getElementById('wordsThreshold')
+const scorebar = document.querySelector(".scorebar")
+const debugToggle = document.getElementById("debug-toggle")
+const fakeToggle = document.getElementById("fake-toggle")
+const unknownToggle = document.getElementById("unknown-toggle")
+const diffStep = document.getElementById('levelDiff')
+const minLetters = document.getElementById('wordLength')
+const LETTERS = 'abcdefghijklmnopqrstuvwxyz'.split('')
+const NO_Q = 'abcdefghijklmnoprstuvwxyz'.split('')
 let count = 0
 
 let intervalId
@@ -16,19 +29,40 @@ class InputHandler {
     constructor(game) {
         this.game = game
         shufButton.addEventListener('click', () => this.game.shuffleTiles())
-            form.addEventListener('submit', (e) => {
+        form.addEventListener('submit', (e) => {
             e.preventDefault()
             if (!input.disabled){
             this.game.check()
             input.value = ""
-        } else if (input.disabled) {
-            form.addEventListener('click', () => count +=1)
-            if (count >=3) {
-                this.game.debug()
-            }
         }
         })
+        debugToggle.addEventListener('change', (e) => {
+            if (e.target.checked) {
+                this.game.debugOn = true
+                this.game.debugInit()
+            } else {
+                this.game.debugOn = false;
+            }
+        })
+        fakeToggle.addEventListener('change', (e) => {
+            this.game.decoy = !this.game.decoy
+            console.log(this.game.decoy)
+        })
+        unknownToggle.addEventListener('change', (e) => {
+            this.game.unknown = !this.game.unknown
+            console.log(this.game.unknown)
+        })
+        diffStep.addEventListener('change', (e) => {
+            this.game.diffStep = e.target.value
+            console.log(this.game.diffStep)
+        })
+        minLetters.addEventListener('change', (e) => {
+            this.game.minLength = e.target.value
+            console.log(this.game.minLength)
+        })
         levelBtn.addEventListener('click', () => this.game.levelUp())
+        debugBtn.addEventListener("click", () => this.game.debug())
+        stopBtn.addEventListener("click", () => this.game.stopTimer())
     }
 }
 
@@ -36,7 +70,9 @@ class WordSet {
     constructor(game, words) {
         this.game = game
         this.words = words
+        this.wordsByLevel = {}
         this.word = ""
+        this.wordInfo = {}
         this.wordSet = {}
         this.sorted = {}
         this.weights = []
@@ -44,18 +80,39 @@ class WordSet {
         this.maxWeight = 10
         this.letters = []
         this.numTiles = 0
+        // this.getWordsByLevel()
     }
-    getRandomWord() {
-    const keys = Object.keys(this.words)
+    // getWordsByLevel() {
+    //     const first = this.words.filter(word => word.difficulty === 1)
+    //     const second = this.words.filter(word => word.difficulty === 2)
+    //     this.wordsByLevel[1] = first
+    //     this.wordsByLevel[2] = second
+    // }
+
+    getRandomWord(difficulty) {
+    const keys = this.words.map(word => word.key)
     const randIndex = Math.floor(Math.random() * keys.length)
     this.word = keys[randIndex]
     this.letters = Array.from(this.word)
-    this.wordSet = words[this.word]
-    console.log(this.wordSet)
-    delete this.words.word
+    this.wordInfo = this.words.find(word => word.key === this.word)
+    this.wordSet = this.wordInfo["words"].filter(word => word.length >= this.game.minLength)
+    if (this.game.debugOn) {
+            console.log(this.wordSet)
+        }
+    this.words = this.words.filter(word => word.key != this.word)
     this.sortByLength()
     this.scoreWeights()
     this.shuffle(this.letters)
+    }
+    getWord(word) {
+        this.word = word
+        this.letters = Array.from(this.word)
+        this.wordInfo = this.words.find(word => word.key === this.word)
+        this.wordSet = this.wordInfo.words
+        console.log(this.wordSet)
+        this.sortByLength()
+        this.scoreWeights()
+        this.shuffle(this.letters)
     }
     sortByLength() {
         this.wordSet.forEach(word => {
@@ -107,7 +164,7 @@ class Game {
         this.timeLimit = 10
         this.time = 
         this.lastTime = 0
-        this.gameOver = false;
+        this.isGameOver = false;
         this.wordsFound = []
         this.container = document.querySelector('.tile-container')
         this.blanks = document.querySelector('.blanks')
@@ -115,22 +172,35 @@ class Game {
         this.score = document.getElementById('score')
         this.input = document.getElementById('input')
         this.start = document.getElementById('start')
+        this.wordsScore = document.getElementById('wordsFound')
         this.input.disabled = true
         this.blanksArray = []
         this.tiles = []
+        this.debugOn = false
+        this.difficulty = 1
+        this.decoy = true
+        this.unknown = true
+        this.diffStep = 5
+        this.minLength = 4
     }
     init() {
         this.wordData = new WordSet(this, this.words)
-        this.wordData.getRandomWord()
-        this.curWord = this.wordData.word
-        console.log(this.curWord)
-        delete this.words[this.curWord]
-        this.found = Object.fromEntries(this.wordData.wordLengths.map(word => [word, []]))
-        this.input.disabled = false
-        this.getMaxScore()
-        this.createTiles()
-        this.createBlanks()
-        this.startTimer()
+        if (this.diffStep == 0) {
+            this.difficulty = 4
+            console.log(this.difficulty)
+        }
+            this.wordData.getRandomWord(this.difficulty)
+            this.curWord = this.wordData.word
+            if (this.debugOn) {
+            console.log(this.curWord)
+            }
+            delete this.words[this.curWord]
+            this.found = Object.fromEntries(this.wordData.wordLengths.map(word => [word, []]))
+            this.input.disabled = false
+            this.getMaxScore()
+            this.createTiles()
+            this.createBlanks()
+            this.startTimer()
     }
     getMaxScore() {
         let scoreArray = this.wordData.weights
@@ -140,16 +210,68 @@ class Game {
             let weightIdx = wordLen.indexOf(key.toString())
             this.maxScore += scoreArray[weightIdx] * numWords
         })
+        threshold.textContent = '/' + Math.floor(0.6*this.maxScore)
+        wordsThreshold.textContent = '/' + Math.floor(0.75*this.wordData.wordSet.length)
         
     }
     createTiles() {
         let letters = this.wordData.letters
         let numTiles = letters.length
+        let confuse = ['decoy', 'unknown']
+        let chosen
+        let randIndex
+        let randLetter
+        if ((!this.decoy && !this.unknown) || this.difficulty == 1) {
+            chosen = ''
+        }
+        if (this.difficulty > 1 && this.difficulty < 4) {
+            if (this.decoy && this.unknown) {
+                chosen = confuse[Math.round(Math.random())]
+            } else if (this.decoy && !this.unknown) {
+                chosen = confuse[0]
+            } else if (this.unknown && !this.decoy) {
+                chosen = confuse[1]
+            }
+        }
+        if (this.difficulty == 4 && this.decoy && this.unknown) {
+            chosen = 'both'
+        } else if (this.difficulty == 4 && this.decoy) {
+            chosen = confuse[0]
+        } else if (this.difficulty == 4 && this.unknown) {
+            chosen = confuse[1]
+        }
+        this.container.style.width = 80*numTiles + 'px'
+        if (chosen == 'both') {
+            randIndex = Math.floor(Math.random()*letters.length)
+            if (letters.includes('u')) {
+                randLetter = LETTERS[Math.floor(Math.random()*LETTERS.length)]
+            } else {
+                randLetter = NO_Q[Math.floor(Math.random()*NO_Q.length)]
+            }
+            letters.push(randLetter)
+            numTiles +=1
+        } else if (chosen == 'unknown') {
+            randIndex = Math.floor(Math.random()*letters.length)
+        } else if (chosen == 'decoy') {
+             if (letters.includes('u')) {
+                randLetter = LETTERS[Math.floor(Math.random()*LETTERS.length)]
+            } else {
+                randLetter = NO_Q[Math.floor(Math.random()*NO_Q.length)]
+            }
+            letters.push(randLetter)
+            numTiles +=1
+            console.log(letters)
+        }
         for (let i=0; i < numTiles; i++) {
             const newTile = document.createElement('div')
             newTile.className = 'tile'
-            newTile.id = letters[i]
+            newTile.id = i
             newTile.textContent = letters[i]
+            if (chosen === 'both' || chosen==='unknown') {
+                if (i === randIndex) {
+                    newTile.textContent = '?'
+                }
+            }
             const xPos = i * 80
             newTile.style.transform = `translate(${xPos}px)`
             newTile.dataset.xPos = xPos
@@ -196,18 +318,26 @@ class Game {
         tiles.forEach(tile => {
             tile.remove()
         })
+        input.value = ""
         this.blanksArray = []
         this.tiles = []
         this.wordsFound = []
         this.scoreVal = 0
+        this.maxScore = 0
         this.score.textContent = 0
+        this.wordsScore.textContent = 0
         this.time = 0
     }
     levelUp() {
         this.level += 1
+        if (this.level >= 10) {
+            this.difficulty =2
+        }
         this.levelDisplay.textContent = this.level
-        display.textContent = "03:00"
+        display.textContent = "02:00"
         levelBtn.style.display = 'none';
+        scorebar.classList.remove('shimmer')
+        this.score.classList.remove('up')
         this.reset()
         this.init()
     }
@@ -220,13 +350,22 @@ class Game {
             this.found[key].push(input)
             let scoreIdx = this.wordData.wordLengths.indexOf(key.toString())
             this.scoreVal += this.wordData.weights[scoreIdx]
-            if (key === this.curWord.length) {
-                this.scoreVal += 10
+            this.wordsScore.textContent = this.wordsFound.length
+            // if (this.debugOn) {
+            //     console.log(this.wordData.weights[scoreIdx])
+            // }
+            if (this.scoreVal >= Math.floor(0.6*this.maxScore) || this.wordsFound.length >= Math.floor(0.75*this.wordData.wordSet.length)) {
+                scorebar.classList.add('shimmer')
+                scoreInfo.classList.add('up')
             }
             let wordIdx = this.wordData.sorted[key].indexOf(input)
             let lettersId = `${key}-${wordIdx}`
             let lettersFound = document.getElementById(lettersId)
             lettersFound.textContent = input
+            if (key === this.curWord.length) {
+                this.scoreVal += 10
+                lettersFound.classList.add('pulse-text')
+            }
             this.score.textContent = this.scoreVal
             if (this.wordsFound.length === this.wordData.wordSet.length) {
                 this.stopTimer()
@@ -236,36 +375,39 @@ class Game {
     }
     scoreLevel() {
         if (this.scoreVal >= Math.floor(0.6*this.maxScore)) {
-            display.textContent = "03:00"
+            display.textContent = "02:00"
             levelBtn.style.display = 'inline-block'
             // start.disabled = false
             // this.levelUp()
         } else if (this.wordsFound.length >= Math.floor(0.75*this.wordData.wordSet.length)) {
-            console.log("found enough words")
-            display.textContent = "03:00"
+            display.textContent = "02:00"
             levelBtn.style.display = 'inline-block'
         } else {
-            this.gameOver = true
-            console.log(0.6*this.maxScore)
-            console.log(Math.floor(0.75*this.wordData.wordSet.length))
+            this.isGameOver = true
+            this.gameOver()
         }
     }
     startTimer() {
-        let timer = 180, min, sec;
+        let timer = 120, min, sec;
         if (!intervalId) {
-        start.disabled = true
+        start.style.display = 'none'
         this.running = true;
+
         intervalId = setInterval(() => {
             min = parseInt(timer/60, 10)
             sec = parseInt(timer % 60, 10)
             min = min < 10 ? "0" + min : min;
             sec = sec < 10 ? "0" + sec : sec;
             display.textContent = min + ":" + sec;
-            if (timer % 15 == 0 && timer!= 180) {
+            const percentage = (timer/120) * 100;
+            remaining.style.width = percentage + '%'
+            if (timer % 15 == 0 && timer!= 120) {
                 this.shuffleTiles()
             }
             if (timer <= 0) {
-                console.log("time's up")
+                if (this.debugOn) {
+                    console.log("time's up")
+                }
                 this.stopTimer()
                 this.scoreLevel()
             }
@@ -277,25 +419,52 @@ class Game {
         clearInterval(intervalId)
         this.running = false
         input.disabled = true
+        if (!this.debugOn) {
+             this.scoreLevel()
+            }
         intervalId = null
     }
+    gameOver() {
+        const main = document.querySelector('.main')
+        main.textContent = ''
+        const restart = document.createElement('button')
+        restart.className = 'btn'
+        restart.textContent = 'Game Over! Try Again?'
+        main.appendChild(restart)
+        restart.addEventListener('click', () => {window.location.reload()})
+
+    }
+    debugInit() {
+        if (this.debugOn) {
+            debugContainer.style.display = 'inline-block'
+        stopBtn.style.display = 'inline-block'
+                if (this.diffStep == 0) {
+            this.difficulty = 4
+            console.log(this.difficulty)
+        }
+    }
+        if (!this.debugOn) {
+            debugContainer.style.display = 'none'
+            stopBtn.style.display = 'none'
+        }
+    }
     debug() {
-        debugContainer.style.display = 'inline-block'
-
+            const debugVal = document.getElementById("debug")
+            const debugInput = debugVal.value.trim().toLowerCase()
+            this.wordData = new WordSet(this, this.words)
+            this.wordData.getWord(debugInput)
+            this.curWord = this.wordData.word
+            console.log(this.curWord)
+            this.found = Object.fromEntries(this.wordData.wordLengths.map(word => [word, []]))
+            this.input.disabled = false
+            this.getMaxScore()
+            this.createTiles()
+            this.createBlanks()
+            this.startTimer()
     }
     }
 
 
-window.addEventListener('load', () => {
-    const game = new Game(words)
-
-    start.addEventListener('click', () => {
-
-        //TODO put gameLoop inside here?
-        game.init()
-
-        })
-})
-
+export { Game, InputHandler, WordSet, shufButton, start, input, form, display, levelBtn, debugContainer, debugBtn, stopBtn, count, intervalId}
 
 
